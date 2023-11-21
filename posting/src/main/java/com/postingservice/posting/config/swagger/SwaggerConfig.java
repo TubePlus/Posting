fcf04b0c9fe597e519506ce3f72e6bb7dbd55dc8
goeom77 <7eom14@gmail.com>
2023-11-21 12:20:10 +0900

    setting commit

---
+++ posting/src/main/java/com/postingservice/posting/config/swagger/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package com.postingservice.posting.config.swagger;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import lombok.RequiredArgsConstructor;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @io.swagger.v3.oas.annotations.info.Info(
+                title = "TubePlus App API",
+                version = "v1",
+                description = "TubePlus App API Docs"
+        )
+)
+@RequiredArgsConstructor
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public GroupedOpenApi publicApi() {
+
+        String[] paths = {"/api/v1/etc/**"};
+
+        return GroupedOpenApi.builder()
+                .group("public-api")
+                .pathsToMatch(paths)
+                .build();
+    }
+}
